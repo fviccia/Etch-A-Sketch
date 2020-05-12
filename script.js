92dc8bdb0712fceb3ab5608bd7d7be88a515d28e
Franco Viccia <Francoviccia@gmail.com>
2020-05-12 12:11:02 -0300

    FInal Version

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ let container = document.getElementById('container');
 createDivs(16,16);
 let gridButton = document.getElementById('grid-selector');
 let resetButton = document.getElementById('reset');
+let colorButton = document.getElementById('colorize');
+let blackButton = document.getElementById('black');
+let eraseButton = document.getElementById('erase');
 
 gridButton.addEventListener('click', changeGrid);
 resetButton.addEventListener('click', resetGrid);
+colorButton.addEventListener('click', randomColor);
+blackButton.addEventListener('click', blackBox);
+eraseButton.addEventListener('click', eraseBox);
 
+
+// Creates divs and append them to the container.
 function createDivs (rows, columns) {
   container.innerHTML = " ";
   let amountOfDivs = rows*columns; 
@@ -16,7 +24,7 @@ function createDivs (rows, columns) {
     container.appendChild(div);
   }
   gridTemplate(rows, columns);
-  mouseOver();
+  blackBox();
 }
 // Changes the grid template according to the createDivs input.
 function gridTemplate (rows, columns) {
@@ -29,17 +37,47 @@ function changeGrid () {
   let columns = window.prompt('Enter the new number of columns.');
   createDivs (rows, columns);
 }
-// This function adds the active class when the mouse is over the box.
-function mouseOver() {
-  document.querySelectorAll('.box').forEach(item => {
-    item.addEventListener('mouseover', event => {
-      item.classList.add('active');
-    })
-  })
-}
-// This function resets the grid, removing the active class.
+
+// Resets the grid establishing all the box to white and returning to black boxes.
+
 function resetGrid () {
-  document.querySelectorAll('.box').forEach(item => {
-    item.classList.remove('active');
-  })
+  let boxes = document.querySelectorAll('.box');
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.cssText = 'background-color: rgb(255, 255, 255);';
+  }
+  blackBox();
+}
+
+
+// Erases the color changing it to white.
+
+function eraseBox () {
+  let boxes = document.querySelectorAll('.box');
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].onmouseover = function(e) {
+        this.style['background-color'] = 'rgb(255, 255, 255)';
+    }  
+  }
+  
+}
+
+//Define a random color to the boxes.
+function randomColor () {
+  let boxes = document.querySelectorAll('.box');
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].onmouseover = function(e) {
+        let color = '#'+Math.floor(Math.random()*16777215).toString(16);
+        this.style['background-color'] = color;
+    }  
+  }
+}
+
+//Return black color to the boxes.
+function blackBox () {
+  let boxes = document.querySelectorAll('.box');
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].onmouseover = function(e) {
+        this.style['background-color'] = 'black';
+    }  
+  }
 }
